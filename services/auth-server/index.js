6af710055423c814aff0405fdd7559652585517c
James Peiris <jamespeiris@hotmail.com>
2019-04-07 10:34:34 +0100

    Added implementation to pass login tests

--- services/auth-server/index.js
+++ services/auth-server/index.js
@@ -1,14 +1,23 @@
 const koa = require('koa');
-
-const app = new koa();
+const bodyParser = require('koa-bodyparser');
 
 function responseHandler(ctx, next) {
-  ctx.cookies.set('session', 'THE_JWT');
-  ctx.status = 204;
+  const { password } = ctx.request.body;
+
+  if (password === 'johns_password') {
+    ctx.cookies.set('session', 'THE_JWT');
+    ctx.status = 204;
+  } else {
+    ctx.set('WWW-Authenticate', 'Dazn');
+    ctx.status = 401;
+  }
 
   return next();
 }
 
+const app = new koa();
+
+app.use(bodyParser());
 app.use(responseHandler);
 
 module.exports = app.listen(3000);
