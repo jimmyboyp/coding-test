706c165731d2cd67314d4aa85331506630a37df4
James Peiris <jamespeiris@hotmail.com>
2019-04-07 11:14:18 +0100

    Refactored koa handler registration to read more sequentially

--- services/auth-server/index.js
+++ services/auth-server/index.js
@@ -3,40 +3,43 @@ const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 const cookieParser = require('koa-cookie').default;
 
-function authHandler(ctx, next) {
-  const { password } = ctx.request.body;
-
-  if (password === 'johns_password') {
-    ctx.cookies.set('session', 'THE_JWT');
-    ctx.status = 204;
-  } else {
-    ctx.set('WWW-Authenticate', 'Dazn');
-    ctx.status = 401;
-  }
-
-  return next();
-}
-
-function sessionHandler(ctx, next) {
-  const { session } = ctx.cookie;
-
-  if (session === 'THE_JWT') {
-    ctx.status = 204;
-  } else {
-    ctx.set('WWW-Authenticate', 'Dazn');
-    ctx.status = 401;
-  }
-
-  return next();
-}
-
 const app = new Koa();
 const router = new Router();
 
 router.use(cookieParser());
 
-router.post('/login', authHandler);
-router.post('/session/check', sessionHandler);
+router.post(
+  '/login',
+  function authHandler(ctx, next) {
+    const { password } = ctx.request.body;
+
+    if (password === 'johns_password') {
+      ctx.cookies.set('session', 'THE_JWT');
+      ctx.status = 204;
+    } else {
+      ctx.set('WWW-Authenticate', 'Dazn');
+      ctx.status = 401;
+    }
+
+    return next();
+  }
+);
+
+router.post(
+  '/session/check',
+  function sessionHandler(ctx, next) {
+    const { session } = ctx.cookie;
+
+    if (session === 'THE_JWT') {
+      ctx.status = 204;
+    } else {
+      ctx.set('WWW-Authenticate', 'Dazn');
+      ctx.status = 401;
+    }
+
+    return next();
+  }
+);
 
 app.use(bodyParser());
 app.use(router.routes());
