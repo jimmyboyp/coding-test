48b5755ff1abcca4cef3c0e595fa00ee07d3a15e
James Peiris <jamespeiris@hotmail.com>
2019-04-07 10:11:09 +0100

    Added session cookie with static JWT value and associated status code

--- services/auth-server/index.js
+++ services/auth-server/index.js
@@ -2,4 +2,12 @@ const koa = require('koa');
 
 const app = new koa();
 
+function responseHandler(ctx, next) {
+  ctx.cookies.set('session', 'THE_JWT');
+  ctx.status = 204;
+  return next()
+}
+
+app.use(responseHandler);
+
 module.exports = app.listen(3000);
